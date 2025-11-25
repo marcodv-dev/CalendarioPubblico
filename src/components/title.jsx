154f510c5633fa8b15967f9a7255509977280e2a
Marco Dalla Valle <marco.dallavalle30@gmail.com>
2025-11-25 01:41:49 +0100

    update jsx 19

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -12,14 +12,14 @@ export const Title =({mostraBack})=>{
 
     return(
         <div className="Titolo">
-            {mostraBack && <div className='esterno'>
+            {mostraBack && <div className='esterno uno'>
                 <button className="back" onClick={back}><img src={Back} alt="" /></button>
             </div>}
             <div>
                 <h1>2° calendario dell'avvento</h1>
                 <h2>Anna Nodari - 2025</h2>
             </div>
-            {mostraBack && <div className='esterno'></div>}
+            {mostraBack && <div className='esterno due'></div>}
         </div>
     );
 }
